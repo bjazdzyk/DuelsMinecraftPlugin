bb97f982eaa2beee3319607a46a5fa4c52fbbab5
Bartosz Jażdżyk <bartek.jazdzyk.007@gmail.com>
2021-08-02 13:52:21 +0200

    info messages on command /duel

--- src/com/github/bjazdzyk/DuelsPlugin/Main.java
+++ src/com/github/bjazdzyk/DuelsPlugin/Main.java
@@ -3,19 +3,28 @@ package com.github.bjazdzyk.DuelsPlugin;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.github.bjazdzyk.DuelsPlugin.commands.DuelCommand;
+
 import net.md_5.bungee.api.ChatColor;
 
 public class Main extends JavaPlugin{
 	public void onEnable() {
 		Bukkit.getLogger().info("Duels Plugin" +  ChatColor.GREEN + " ENABLED");
+		
 		loadConfig();
 		Bukkit.getLogger().info(ChatColor.GREEN + "Config loaded");
+		
+		this.getCommand("duel").setExecutor(new DuelCommand());
 	}
 	public void onDisable() {
 		Bukkit.getLogger().info("Duels Plugin" +  ChatColor.RED + " DISABLED");
 	}
 	public void loadConfig() {
-		getConfig().addDefault("messages.IncorrectDuelCommandUsage", ChatColor.RED + "INCORRECT usage of command /duel" + "\n" + ChatColor.RESET + "Please type" + ChatColor.BOLD + "/duel <PlayerName>");
+		getConfig().addDefault("messages.DuelOfferSent", "Duel offer sent . . .");
+		getConfig().addDefault("messages.noArgumentsInDuelsCommand", "INCORRECT usage of command /duel" + "\n" + "Please type /duel <PlayerName> <kit>");
+		getConfig().addDefault("messages.DuelOfferRecived", " sent you duel offer");
+		getConfig().addDefault("messages.IsOffline", " is offline now");
+		getConfig().addDefault("messages.FrstArg_PlayerNickname", "First argument of /duels cannot be your nickname!");
 		getConfig().options().copyDefaults(true);
 		saveConfig();
 	}
--- src/com/github/bjazdzyk/DuelsPlugin/commands/DuelCommand.java
+++ src/com/github/bjazdzyk/DuelsPlugin/commands/DuelCommand.java
@@ -1,17 +1,22 @@
 package com.github.bjazdzyk.DuelsPlugin.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 import com.github.bjazdzyk.DuelsPlugin.Main;
 
+import net.md_5.bungee.api.ChatColor;
+
 
 public class DuelCommand implements CommandExecutor{
 
 	private Plugin plugin = Main.getPlugin(Main.class);
+	private FileConfiguration config = plugin.getConfig();
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String str, String[] args) {
@@ -19,9 +24,22 @@ public class DuelCommand implements CommandExecutor{
 			if(sender instanceof Player) {
 				Player player = (Player) sender;
 				if(args.length > 0) {
-					player.sendMessage("LUL");
+					if(plugin.getServer().getOnlinePlayers().contains(Bukkit.getPlayer(args[0]))) {
+						Player oponent = Bukkit.getPlayer(args[0]);
+						if(player.equals(oponent)) {
+							player.sendMessage(ChatColor.RED + config.getString("messages.FrstArg_PlayerNickname"));
+							return true;
+						}
+						player.sendMessage(ChatColor.GOLD + config.getString("messages.DuelOfferSent"));
+						oponent.sendMessage(ChatColor.BOLD + player.getName() + ChatColor.RESET + ChatColor.GOLD +  config.getString("messages.DuelOfferRecived"));
+						return true;
+					}else {
+						player.sendMessage(ChatColor.GOLD + args[0] + ChatColor.RESET + ChatColor.RED + config.getString("messages.IsOffline"));
+						return true;
+					}
 				}else {
-					player.sendMessage(plugin.getConfig().getString("messages.IncorrectDuelCommandUsage"));
+					player.sendMessage(ChatColor.RED + config.getString("messages.noArgumentsInDuelsCommand"));
+					return true;
 				}
 				
 			}
